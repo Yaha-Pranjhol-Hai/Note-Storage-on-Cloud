ecbf3a3f97ecc0372b798b469f1eaec93a9aedb6
Pranjal <jainpranjal30112002@gmail.com>
2023-01-26 14:27:31 +0530

    Everything works.

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
 
 mongoose.set('strictQuery', false);
 
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+const connectToMongo = async ()=>{
+    await mongoose.connect(mongoURI, ()=>{
         console.log("connected to Mongo Successfully");
     })
 }
