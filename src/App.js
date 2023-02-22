d3e3b055815d71f11d537e9b89414b2b002429df
Pranjal <jainpranjal30112002@gmail.com>
2023-02-22 17:42:52 +0530

    Navbar and Rotes added, but Home and About section are not added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
