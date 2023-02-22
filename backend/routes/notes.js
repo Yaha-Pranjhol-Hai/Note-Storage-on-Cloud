820bb1ccf7bddc357bfaa624ded06427dbee017e
Pranjal <jainpranjal30112002@gmail.com>
2023-02-22 16:19:23 +0530

    Update an existing note.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,7 +4,7 @@ const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
 var fetchuser = require("../middleware/fetchuser");
 
-// ROUTE 1: Get all the Notes using: GET "/api/auth/fetchallnotes". Login required
+// ROUTE 1: Get all the Notes using: GET "/api/notes/fetchallnotes". Login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Note.find({ user: req.user.id });
@@ -15,7 +15,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-// ROUTE 2: Add a New Note using: POST "/api/auth/addnote". Login required
+// ROUTE 2: Add a New Note using: POST "/api/notes/addnote". Login required
 router.post(
   "/addnote",
   fetchuser,
@@ -44,11 +44,34 @@ router.post(
 
       const savedNote = await note.save();
       res.json(savedNote);
-
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
     }
+  }
+);
+
+// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote". Login required
+router.put(
+  "/updatenote/:id",fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed"); 
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+
   }
 );
 
